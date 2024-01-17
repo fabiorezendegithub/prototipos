1bf23cdf236479a1c77f913c13e7748df3f85f85
fabio <fabio.rezende@gmail.com>
2024-01-17 18:16:26 -0300

    adicionado models

---
+++ PrototipoRazor/Models/Premium.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace PrototipoRazor.Models;
+
+public class Premium
+{
+    [Key]
+    [DisplayName("Id")]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Informe o título do Premium")]
+    [StringLength(80, ErrorMessage = "O título deve conter até 80 caracteres")]
+    [MinLength(5, ErrorMessage = "O título deve conter pelo menos 5 caracteres")]
+    [DisplayName("Título")]
+    public string Title { get; set; } = string.Empty;
+
+    [DataType(DataType.DateTime)]
+    [DisplayName("Início")]
+    public DateTime StartDate { get; set; }
+
+    [DataType(DataType.DateTime)]
+    [DisplayName("Término")]
+    public DateTime EndDate { get; set; }
+
+    [Required(ErrorMessage = "Aluno inválido")]
+    [DisplayName("Aluno")]
+    public int StudentId { get; set; }
+
+    public Student? Students { get; set; }
+}
---
+++ PrototipoRazor/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrototipoRazor.Models;
+
+public class Student
+{
+    [Key]
+    [DisplayName("Id")]
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Informe o nome do Usuário")]
+    [StringLength(80, ErrorMessage = "Nome deve conter até 80 caracteres")]
+    [MinLength(5 , ErrorMessage = "Nome deve conter pelo menos 5 caracteres")]
+    [DisplayName("Nome Completo")]
+    public string Name { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Informe o E-mail")]
+    [EmailAddress(ErrorMessage = "E-mail inválido")]
+    [DisplayName("E-mail")]
+    public string Email { get; set; } = string.Empty;
+
+    public List<Premium> Premiumns { get; set; } = new();
+}
