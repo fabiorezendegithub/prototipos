3313e1ab84e651d7444b2da9c6ca98fa7f84db38
fabio <fabio.rezende@gmail.com>
2024-01-18 16:30:40 -0300

    prototipoRazor: adiciona create de student

---
+++ PrototipoRazor/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PrototipoRazor.Data;
+using PrototipoRazor.Models;
+
+namespace PrototipoRazor.Pages_Students
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Student Student { get; set; } = default!;
+        
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Students == null || Student == null)
+            {
+                return Page();
+            }
+
+            _context.Students.Add(Student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
